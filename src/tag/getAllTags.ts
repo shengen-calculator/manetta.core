6c57ed64f26bc12cf7bef21559086e46a464bf83
Bass <suddya@toptal.com>
2023-06-28 19:07:07 +0100

    remove create, delete tag func

--- src/tag/getAllTags.ts
+++ src/tag/getAllTags.ts
@@ -6,11 +6,8 @@ export const getAllTags = async (data: any, context: any) => {
     try {
         const datastore = new Datastore();
         const dataStoreService = new DataStoreService(datastore);
-        const tags = await dataStoreService.getAll("tag", true);
-        return tags.map((entity) => {
-            const key = entity[datastore.KEY];
-            return key.name;
-        });
+        const tags = await dataStoreService.getAll("tag", false);
+        return tags.map((data) => data.tags);
     } catch (error: any) {
         const runQueryError: RunQueryError = error;
         throw new functions.https.HttpsError("internal",
