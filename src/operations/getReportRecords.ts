49dfb80f34300e8eeef6ff2b8fa312a94ff0602b
bass <bass@basss-MBP.lan>
2024-08-22 13:51:09 +0300

    fix parameters

--- src/operations/getReportRecords.ts
+++ src/operations/getReportRecords.ts
@@ -7,15 +7,18 @@ export const getReportRecords = async (data: any, context: any) => {
         const datastore = new Datastore();
         const dataStoreService = new DataStoreService(datastore);
         const blockedMap = new Map();
-        const startDate = data.startDate ? new Date(data.startDate) : undefined;
-        const endDate = data.endDate ? new Date(data.endDate) : undefined;
+        const startDate = data.filter.startDate ?
+            new Date(data.filter.startDate) : undefined;
+        const endDate = data.filter.endDate ?
+            new Date(data.filter.endDate) : undefined;
         const accounts = await dataStoreService.getAll("account", false);
         accounts.forEach((entity) => {
             const key = entity[datastore.KEY];
             blockedMap.set(key.name, entity.blocked);
         });
         const result = await dataStoreService
-            .getNewestFilteredItems("posted", data.cursor, startDate, endDate);
+            .getNewestFilteredItems("posted", data.startCursor,
+                startDate, endDate, data.filter.tags);
         const entries = result.entities.map((entity) => {
             const key = entity[datastore.KEY];
             return {
